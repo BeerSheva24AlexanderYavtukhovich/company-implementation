e1a5e6c3a088b471dd141166968392f131a76c1e
Alexander Yavtuhovich <yavtik@gmail.com>
2024-09-19 17:45:03 +0300

    CW30 - init

---
+++ src/main/java/telran/employees/SalesPerson.java
@@ -0,0 +1,17 @@
+package telran.employees;
+
+public class SalesPerson extends WageEmployee {
+    float percent;
+    long sales;
+
+    public SalesPerson(long id, int basicSalary, String department, int wage, int hours, float percent, long sales) {
+        super(id, basicSalary, department, wage, hours);
+        this.percent = percent;
+        this.sales = sales;
+    }
+
+    @Override
+    public int computeSalary() {
+        return (int) (super.computeSalary() + sales * percent);
+    }
+}
